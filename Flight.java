ba527dc78cf04c0409d53dc839d421fda5f078f9
soundu2000 <soundaryah157@gmail.com>
2024-10-18 19:05:18 +0530

    objects

---
+++ Flight.java
@@ -0,0 +1,24 @@
+public class Flight {
+    int flightNumber;;
+    String destination;
+    int availableSeats;
+    double ticketPrice;
+    String status;
+
+    public static void boardPassenger() {
+
+    }
+
+    public static void takeOff() {
+
+    }
+
+    public static void land() {
+
+    }
+
+    public static void updateStatus() {
+
+    }
+
+}
