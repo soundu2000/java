0843d72395fc2857e0366dba64d478344bf5e608
soundu2000 <soundaryah157@gmail.com>
2024-10-18 19:06:35 +0530

    variables

---
+++ FlightInfo.java
@@ -0,0 +1,155 @@
+public class FlightInfo {
+    public static void main(String[] args) {
+
+        Flight flight = new Flight();
+        flight.flightNumber = 101;
+        flight.destination = "Japan";
+        flight.availableSeats = 100;
+        flight.ticketPrice = 15000;
+        flight.status = "On Time";
+
+        System.out.println("Flight Number: " + flight.flightNumber);
+        System.out.println("Flight Destination: " + flight.destination);
+        System.out.println("Seats are available in Flight: " + flight.availableSeats);
+        System.out.println("Ticket Price of Flight: " + flight.ticketPrice);
+        System.out.println("Flight Status: " + flight.status);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        Flight flight01 = new Flight();
+        flight01.flightNumber = 105;
+        flight01.destination = "Manali";
+        flight01.availableSeats = 150;
+        flight01.ticketPrice = 25000;
+        flight01.status = "Delay";
+
+        System.out.println("Flight Number: " + flight01.flightNumber);
+        System.out.println("Flight Destination: " + flight01.destination);
+        System.out.println("Seats are available in Flight: " + flight01.availableSeats);
+        System.out.println("Ticket Price of Flight: " + flight.ticketPrice);
+        System.out.println("Flight Status: " + flight01.status);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        Flight flight02 = new Flight();
+        flight02.flightNumber = 115;
+        flight02.destination = "Korea";
+        flight02.availableSeats = 95;
+        flight02.ticketPrice = 10000;
+        flight02.status = "Landed";
+
+        System.out.println("Flight Number: " + flight02.flightNumber);
+        System.out.println("Flight Destination: " + flight02.destination);
+        System.out.println("Seats are available in Flight: " + flight02.availableSeats);
+        System.out.println("Ticket Price of Flight: " + flight.ticketPrice);
+        System.out.println("Flight Status: " + flight02.status);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        Flight flight03 = new Flight();
+        flight03.flightNumber = 25;
+        flight03.destination = "German";
+        flight03.availableSeats = 195;
+        flight03.ticketPrice = 2000;
+        flight03.status = "Boarding";
+
+        System.out.println("Flight Number: " + flight03.flightNumber);
+        System.out.println("Flight Destination: " + flight03.destination);
+        System.out.println("Seats are available in Flight: " + flight03.availableSeats);
+        System.out.println("Ticket Price of Flight: " + flight.ticketPrice);
+        System.out.println("Flight Status: " + flight03.status);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        Flight flight04 = new Flight();
+        flight04.flightNumber = 225;
+        flight04.destination = "Hydrabad";
+        flight04.availableSeats = 200;
+        flight04.ticketPrice = 2500;
+        flight04.status = "Cancelled";
+
+        System.out.println("Flight Number: " + flight04.flightNumber);
+        System.out.println("Flight Destination: " + flight04.destination);
+        System.out.println("Seats are available in Flight: " + flight04.availableSeats);
+        System.out.println("Ticket Price of Flight: " + flight.ticketPrice);
+        System.out.println("Flight Status: " + flight04.status);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        Flight flight05 = new Flight();
+        flight05.flightNumber = 220;
+        flight05.destination = "Pune";
+        flight05.availableSeats = 50;
+        flight05.ticketPrice = 1000;
+        flight05.status = "On Time";
+
+        System.out.println("Flight Number: " + flight05.flightNumber);
+        System.out.println("Flight Destination: " + flight05.destination);
+        System.out.println("Seats are available in Flight: " + flight05.availableSeats);
+        System.out.println("Ticket Price of Flight: " + flight.ticketPrice);
+        System.out.println("Flight Status: " + flight05.status);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        Flight flight06 = new Flight();
+        flight06.flightNumber = 105;
+        flight06.destination = "New York";
+        flight06.availableSeats = 75;
+        flight06.ticketPrice = 18000;
+        flight06.status = "Delay";
+
+        System.out.println("Flight Number: " + flight06.flightNumber);
+        System.out.println("Flight Destination: " + flight06.destination);
+        System.out.println("Seats are available in Flight: " + flight06.availableSeats);
+        System.out.println("Ticket Price of Flight: " + flight.ticketPrice);
+        System.out.println("Flight Status: " + flight06.status);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        Flight flight07 = new Flight();
+        flight07.flightNumber = 205;
+        flight07.destination = "Mumbai";
+        flight07.availableSeats = 175;
+        flight07.ticketPrice = 21000;
+        flight07.status = "Landed";
+
+        System.out.println("Flight Number: " + flight07.flightNumber);
+        System.out.println("Flight Destination: " + flight07.destination);
+        System.out.println("Seats are available in Flight: " + flight07.availableSeats);
+        System.out.println("Ticket Price of Flight: " + flight.ticketPrice);
+        System.out.println("Flight Status: " + flight07.status);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        Flight flight08 = new Flight();
+        flight08.flightNumber = 38;
+        flight08.destination = "Goa";
+        flight08.availableSeats = 145;
+        flight08.ticketPrice = 5000;
+        flight08.status = "Boarding";
+
+        System.out.println("Flight Number: " + flight08.flightNumber);
+        System.out.println("Flight Destination: " + flight08.destination);
+        System.out.println("Seats are available in Flight: " + flight08.availableSeats);
+        System.out.println("Ticket Price of Flight: " + flight.ticketPrice);
+        System.out.println("Flight Status: " + flight08.status);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        Flight flight09 = new Flight();
+        flight09.flightNumber = 115;
+        flight09.destination = "Italy";
+        flight09.availableSeats = 65;
+        flight09.ticketPrice = 8000;
+        flight09.status = "Cancelled";
+
+        System.out.println("Flight Number: " + flight09.flightNumber);
+        System.out.println("Flight Destination: " + flight09.destination);
+        System.out.println("Seats are available in Flight: " + flight09.availableSeats);
+        System.out.println("Ticket Price of Flight: " + flight.ticketPrice);
+        System.out.println("Flight Status: " + flight09.status);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+    }
+
+}
