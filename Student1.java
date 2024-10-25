26474cc5272072a4e21498850af29d889c54daf0
soundu2000 <soundaryah157@gmail.com>
2024-10-25 10:44:30 +0530

    static

---
+++ Student1.java
@@ -0,0 +1,99 @@
+public class Student1 {
+       public static void main(String args[]) {
+       
+       Student student = new Student("Ramesh", 'A', 8.1, 47); 
+       student.printStudentDetails();
+
+       Student student1 = new Student("Suresh", 'B', 7.1, 67);
+       student1.printStudentDetails();
+
+       Student student2 = new Student("Sahana", 'C', 8.1, 167);
+       student2.printStudentDetails();
+
+       Student student3 = new Student("Santhosh", 'D', 8.1, 87);
+       student3.printStudentDetails();
+
+       Student student4 = new Student("Seena", 'E', 9.1, 37);
+       student4.printStudentDetails();
+
+       LibraryBook librarybook = new LibraryBook("The Alchemist's Secret Dairy", "Gosling","The Silent Horizon", true);
+       librarybook.printLibraryBookInfo();
+
+       LibraryBook librarybook1 = new LibraryBook("Echoes of a Broken Past", "John","The Lost Chronicles", false);
+       librarybook1.printLibraryBookInfo();
+
+       LibraryBook librarybook2 = new LibraryBook("Beneath the Starlit Canopy", "Gosling","A Journey Through Time", true);
+       librarybook2.printLibraryBookInfo();
+ 
+       LibraryBook librarybook3 = new LibraryBook("The Alchemists Secret Dairy", "Gayle","Rise of Fallen Kings", false);
+       librarybook2.printLibraryBookInfo();
+
+       LibraryBook librarybook4 = new LibraryBook("Whispers of the Forgotten Shores", "james","Shadows of Eternity", false);
+       librarybook4.printLibraryBookInfo();
+
+       Employee employee = new Employee("Kavya", 27, "HR", 25000);
+       employee.printEmployeeInfo();
+    
+       Employee employee1 = new Employee("Keerthi", 25, "HR", 28000);
+       employee1.printEmployeeInfo();
+
+       Employee employee2 = new Employee("Kanaka", 26, "HR", 27000);
+       employee2.printEmployeeInfo();
+
+       Employee employee3 = new Employee("Sahana", 24, "HR", 35000);
+       employee3.printEmployeeInfo();
+
+       Employee employee4 = new Employee("Kruthi", 37, "HR", 35000);
+       employee4.printEmployeeInfo();
+
+       Car car = new Car("Ford", "White", 2014, 35);
+       car.printCarInfo();
+
+       Car car1 = new Car("Nissan", "Blue", 2011, 28);
+       car1.printCarInfo();
+
+       Car car2 = new Car("Audi", "Black", 2012, 35);
+       car2.printCarInfo();
+
+       Car car3 = new Car("BMW", "White", 2010, 28);
+       car3.printCarInfo();
+
+       Car car4 = new Car("Toyota", "White", 2009, 35);
+       car4.printCarInfo();
+
+       Bank bank = new Bank("Soundu", 100000, "savings", 345679863);
+       bank.printBankDetails();
+     
+       Bank bank1 = new Bank("Sonu", 200000, "current", 425759863);
+       bank1.printBankDetails();
+
+       Bank bank2 = new Bank("Shivu", 300000, "savings", 475479863);
+       bank2.printBankDetails();
+
+       Bank bank3 = new Bank("Shyam", 400000, "current", 565679863);
+       bank3.printBankDetails();
+
+       Bank bank4 = new Bank("Shilpa", 400000, "savings", 345679856);
+       bank4.printBankDetails();
+    }
+}
+
+       
+
+
+       
+
+
+
+
+
+
+
+       
+
+
+
+
+
+
+     
