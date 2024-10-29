c892abfe352259d1c67ba7745fe4686ca219e28f
soundu2000 <soundaryah157@gmail.com>
2024-10-29 09:10:47 +0530

    Chaining

---
+++ Students.java
@@ -0,0 +1,9 @@
+public class Students {
+     public static void main(String args[]) {
+     Employees students = new Employees("Soundu", 101, 20000);
+ 
+   students.EmployeeInfo();
+    
+    }
+}
+    
